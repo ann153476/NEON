b7d0ce67d8d506960d2bf73ce12857c3ec2eb150
Anka <103377387+ann153476@users.noreply.github.com>
2023-02-12 18:26:35 +0200

    new

--- src/js/map.js
+++ src/js/map.js
@@ -1,9 +1,18 @@
 let map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 48.51029069230209, lng: 34.61206165767066 },//48.51029069230209, 34.61206165767066
+    let positionMy ={ lat: 48.51029069230209, lng: 34.61206165767066 }
+    map = new google.maps.Map(document.getElementById("map"), {
+    center: positionMy,
     zoom: 10,
+
+  });
+  const image =
+  "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
+  const beachMarker = new google.maps.Marker({
+    position: positionMy,
+    map,
+    icon: image,
   });
 }
 
